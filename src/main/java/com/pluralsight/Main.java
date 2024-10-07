2d5e52755de2b3b46eff7be80021dad0e4cf7676
Xavier Glenn <xglenn@appdev.yearup.org>
2024-10-07 11:09:50 -0700

    Car models, updated

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,14 +1,39 @@
 package com.pluralsight;
 
 public class Main {
-    public static void main(String[] args)
-    {
+
+    private String purpose;
+    private String model;
+    private String color;
+    private double price;
+    private String year;
+
+    public static void main(String[] args) {
         class car {
             public car(String s, String black, int i) {
             }
-        car c1 = new car("Mitsubishi Eclipse", "Black", 16000);
-        car c2 = new car("Dodge Viper", "Black", 60000);
-        car c3 = new car("Nissan 350z", "Black", 30000);
+
+            car c1 = new car("Mitsubishi Eclipse", "Black", 16000);
+            car c2 = new car("Dodge Viper", "Black", 60000);
+            car c3 = new car("Nissan 350z", "Black", 30000);
+        }
+    }
+
+    public void carInfo() {
+        System.out.printf("Model: " + this.model + "\n" +
+                "Type: " + this.model + "\n" +
+                "Year: " + this.year + "\n" +
+                "Color: " + this.color + "\n" +
+                "Weight: " + this.purpose + "\n" +
+                "Price: $%.2f", this.price);
+
+        {
+            if (price < 0) {
+                throw new IllegalArgumentException("Price cannot be negative");
+            }
+            this.price = price;
+            {
+            }
         }
     }
 }
--- src/main/java/com/pluralsight/car.java
+++ src/main/java/com/pluralsight/car.java
@@ -1,42 +1,98 @@
-public void main() {
-}
+package com.pluralsight;
 
-//Variables
-private String model;
-private String price;
-private String color;
-private String purpose;
-private int year;
+public class car {
+    // Field attributes
+    private final String model;
+    private double weightInPounds;
+    private double price;
+    private String color;
+    private final String purpose;
+    private final int year;
 
-//this.-
-public car(String _model, String _color, String _price, int _year, String _purpose) {
-    this.model = _model;
-    this.color = _color;
-    this.price = _price;
-    this.purpose = _purpose;
-    this.year = _year;
-}
+    // Constructor
+    public car(String model, String color, double price, double weightInPounds, String purpose, int year) {
 
-//Empty constructor
-public car()
-{
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        if (weightInPounds < 0) {
+            throw new IllegalArgumentException("Weight cannot be negative");
+        }
 
-}
-public String getModel() {
-    return model;
-}
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.weightInPounds = weightInPounds;
+        this.purpose = purpose;
+        this.year = year;
+    }
+    // Empty constructor
+    public car() {
+        this.model = "Unknown";
+        this.color = "Unknown";
+        this.price = 0.0;
+        this.weightInPounds = 0.0;
+        this.purpose = "Unknown";
+        this.year = 0;
+    }
 
-public String getColor() {
-    return color;
-}
-public String getPurpose() {
-    return purpose;
-}
+    // Getters
+    public String getModel() {
+        return model;
+    }
 
-public int getYear() {
-    return year;
-}
+    public String getColor() {
+        return color;
+    }
 
-public String getPrice(double _price) {
-    return price;
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getWeightInPounds() {
+        return weightInPounds;
+    }
+
+    // Setters
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        this.price = price;
+    }
+    public void setWeightInPounds(double weightInPounds) {
+        if (weightInPounds < 0) {
+            throw new IllegalArgumentException("Weight cannot be negative");
+        }
+        this.weightInPounds = weightInPounds;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Display car information
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", weightInPounds=" + weightInPounds +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", purpose='" + purpose + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public void carInformation() {
+        System.out.println(this);
+    }
 }
